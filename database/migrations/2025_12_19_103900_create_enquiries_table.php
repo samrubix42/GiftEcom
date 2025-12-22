33c09f1d240eb2b5eeb69ed14b930276a4894bcd
Saumy anand <samcool3203@gmail.com>
2025-12-21 20:59:20 -0800

    status added in enum in enquires for status management

--- database/migrations/2025_12_19_103900_create_enquiries_table.php
+++ database/migrations/2025_12_19_103900_create_enquiries_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->text('message')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
             $table->timestamps();
         });
     }
